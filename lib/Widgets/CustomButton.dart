4a5b08576c2480f8b4d4f996732ff6a5affc0787
abh1navkrishna <abhinavkrishnamannarath@gmail.com>
2024-07-06 10:50:08 +0530

    first commit

---
+++ lib/Widgets/CustomButton.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../Contant/Color.dart';
+import 'App Text.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key, //Custom Button......
+    required this.btnname,
+    required this.btntheam,
+    required this.textcolor,
+    required this.click,
+    this.height = 50,
+    this.textsize = 16,
+    this.outline = false,
+  });
+
+  final String btnname;
+  final Color btntheam;
+  final Color textcolor;
+  final double height;
+  final double textsize;
+  final bool outline;
+  final void Function() click;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: click,
+      child: Container(
+        height: height.h,
+        width: double.infinity.w,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8).r,
+          border: Border.all(color: outline == true ? customBlack2 : btntheam),
+          color: btntheam,
+        ),
+        child: Center(
+          child: AppText(
+              text: btnname,
+              textcolor: textcolor,
+              size: textsize.sp,
+              weight: FontWeight.w700),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton1 extends StatelessWidget {
+  const CustomButton1({
+    super.key, //Custom Button......
+    required this.btnname,
+    required this.btntheam,
+    required this.textcolor,
+    required this.click,
+    this.height = 50,
+    this.textsize = 16,
+    this.outline = false,
+    required this.icon,
+  });
+
+  final String btnname;
+  final Color btntheam;
+  final Color textcolor;
+  final double height;
+  final double textsize;
+  final bool outline;
+  final IconData icon;
+  final void Function() click;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: click,
+      child: Container(
+        height: height.h,
+        width: double.infinity.w,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8).r,
+          border: Border.all(color: outline == true ? customBlack2 : btntheam),
+          color: btntheam,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon),
+            SizedBox(
+              width: 10.h,
+            ),
+            AppText(
+                text: btnname,
+                textcolor: textcolor,
+                size: textsize.sp,
+                weight: FontWeight.w700),
+          ],
+        ),
+      ),
+    );
+  }
+}
